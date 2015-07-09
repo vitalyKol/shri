f45574052df11e96197cbedc996994aa3ad1ef5a
Alexey <choudalex@gmail.com>
2015-07-10 02:00:08 +0300

    task-1 buttons, filter and colors

--- task-1/app.js
+++ task-1/app.js
@@ -1,8 +1,34 @@
-var data = [];
+var data = [
+    {
+        type: 1,
+        number: 120,
+        company: 'Аэрофлот',
+        logo: 'http://www.ljplus.ru/img4/f/e/fencer_dv/aeroflot-logo2.png',
+        aircraft: 'aerobus'
+    }
+];
 
-(function() {
+$(document).ready(function() {
+
+    var stickyNavTop = $('table thead').offset().top,
+        element = null;
+
+    var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            if (!element) { element = $('table thead').clone(true).appendTo($('table')).addClass('sticky'); }
+        } else {
+            element && element.remove();
+            element = null;
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
 
 
-
-
-})();
+});

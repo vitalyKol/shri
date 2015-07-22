5d67299669e7c5bbf2d0516c86783a5f96f9116e
AlexeyChudinov <choudalex@gmail.com>
2015-07-22 20:11:03 +0300

    task-1 scss refactor

--- task-1/app.js
+++ task-1/app.js
@@ -32,14 +32,14 @@ function isStickySupported () {
 
 function rowTemplate (item) {
     return '<tr class="' + item.type + '">\
-                <td class="type" style="background-image: url(img/' + item.type + '.png);"></td>\
+                <td class="board__item--type" style="background-image: url(img/' + item.type + '.png);"></td>\
                 <td>\
                     <a href="#flight' + item.number + '">' + item.number + '</a>\
                     <div class="popup" id="flight' + item.number + '">' + popupTemplate(item) + '</div>\
                 </td>\
-                <td class="company">' + item.company + '</td>\
-                <td class="logo"><img src="img/' + item.logo + '.png" /></td>\
-                <td class="aircraft">\
+                <td class="board__item--company">' + item.company + '</td>\
+                <td class="board__item--logo"><img src="img/' + item.logo + '.png" /></td>\
+                <td class="board__item--aircraft">\
                     <span data-full="' + item.aircraft_full + '" data-short="' + item.aircraft_short + '"></span>\
                 </td>\
                 <td>' + item.destination + '</td>\
@@ -52,42 +52,42 @@ function rowTemplate (item) {
 function popupTemplate (item) {
     var type = (item.type == 'arrival') ? 'прибытие' : 'отправление';
 
-    return '<a href="#" class="popup-overlay"></a>\
+    return '<a href="#" class="popup__overlay"></a>\
             <div class="popup-content">\
                 <h2>\
                     <span class="red">И</span>нформация о рейсе № ' + item.number + '\
                     <img src="img/' + item.type + '.png" width="30" />\
                 </h2>\
                 <div><img src="img/' + item.logo + '.png" /></div>\
-                <a href="#" class="popup-close">Закрыть</a>\
-                <ul>\
+                <a href="#" class="popup__close">Закрыть</a>\
+                <ul class="params-list">\
                     <li>\
-                        <span class="popup-item-title">Тип рейса:</span>\
-                        <span class="popup-item-value">' + type + '</span>\
+                        <span class="params-list__title">Тип рейса:</span>\
+                        <span class="params-list__value">' + type + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Авиакомпания:</span>\
-                        <span class="popup-item-value">' + item.company + '</span>\
+                        <span class="params-list__title">Авиакомпания:</span>\
+                        <span class="params-list__value">' + item.company + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Тип воздушного судна:</span>\
-                        <span class="popup-item-value">' + item.aircraft_full + '</span>\
+                        <span class="params-list__title">Тип воздушного судна:</span>\
+                        <span class="params-list__value">' + item.aircraft_full + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Аэропорт назначения:</span>\
-                        <span class="popup-item-value">' + item.destination + '</span>\
+                        <span class="params-list__title">Аэропорт назначения:</span>\
+                        <span class="params-list__value">' + item.destination + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Время по расписанию:</span>\
-                        <span class="popup-item-value">' + item.time + '</span>\
+                        <span class="params-list__title">Время по расписанию:</span>\
+                        <span class="params-list__value">' + item.time + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Статус рейса:</span>\
-                        <span class="popup-item-value">' + item.status + '</span>\
+                        <span class="params-list__title">Статус рейса:</span>\
+                        <span class="params-list__value">' + item.status + '</span>\
                     </li>\
                     <li>\
-                        <span class="popup-item-title">Примечание:</span>\
-                        <span class="popup-item-value">' + item.comment + '</span>\
+                        <span class="params-list__title">Примечание:</span>\
+                        <span class="params-list__value">' + item.comment + '</span>\
                     </li>\
                 </ul>\
             </div>'

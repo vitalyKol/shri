20f36e00509788afa16490e70c7379376f5cf137
AlexeyChudinov <choudalex@gmail.com>
2015-07-23 20:02:47 +0300

    task-3 added module js pattern, layout

--- task-3/app.js
+++ task-3/app.js
@@ -1,42 +1,83 @@
-var audio = new Audio();
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
-var volume = context.createGain();
+'use strict';
 
-var fileInput = document.querySelector('input[type=file]');
-var volumeInput = document.querySelector('input[type=range]');
-var playButton = document.getElementById('play');
-var stopButton = document.getElementById('stop');
+var audioPlayer = (function () {
 
-fileInput.onchange = handleFileUpload;
-volumeInput.oninput = handleVolumeChange;
-playButton.onclick = handlePlayClick;
-stopButton.onclick = handleStopClick;
+    var fileInput, volumeInput, playButton, stopButton, dropzone, playlist;
 
-function handleVolumeChange (e) {
-    volume.gain.value = e.target.value;
-}
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+        volume = context.createGain(),
+        audio = new Audio(),
+        source;
 
-function handleFileUpload (e) {
-    e.stopPropagation();
-    e.preventDefault();
+    var filesList = [];
 
-    var files = e.target.files;
-    audio.src = URL.createObjectURL(files[0]);
+    function init () {
+        fileInput = document.querySelector('input[type=file]');
+        volumeInput = document.querySelector('input[type=range]');
+        playButton = document.querySelector('.play');
+        stopButton = document.querySelector('.stop');
+        dropzone = document.querySelector('.dropzone');
+        playlist = document.querySelector('.player__playlist ul');
 
-}
+        source = context.createMediaElementSource(audio);
+        source.connect(volume);
 
-function handlePlayClick() {
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
+        document.addEventListener('drop', function (e) { e.preventDefault(); });
+        document.addEventListener('dragover', function (e) { e.preventDefault(); }); 
+
+        setupListeners();
     }
-}
 
-function handleStopClick() {
-    if (!audio.paused) {
-        audio.pause();
-        audio.currentTime = 0;
+    function setupListeners () {
+        fileInput.onchange = handleFileUpload;
+        volumeInput.oninput = handleVolumeChange;
+        playButton.onclick = handlePlayClick;
+        stopButton.onclick = handleStopClick;
+
+        dropzone.ondrop = handleFileUpload;
     }
-}
+
+    function handleVolumeChange (e) {
+        volume.gain.value = e.target.value;
+    }
+
+    function handleFileUpload (e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var files;
+
+        if (e.target.nodeName === "DIV") {
+            files = e.dataTransfer.files;
+        } else if (e.target.nodeName === "INPUT") {
+            files = e.target.files;
+        }
+
+        audio.src = URL.createObjectURL(files[0]);
+
+        playlist.innerHTML += ('<li>' + files[0].name + '</li>');
+        filesList.push(files[0]);
+    }
+
+    function handlePlayClick () {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    }
+
+    function handleStopClick () {
+        if (!audio.paused) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    }
+
+    return {
+        init: init
+    }
+
+})();
+
+window.onload = audioPlayer.init;
